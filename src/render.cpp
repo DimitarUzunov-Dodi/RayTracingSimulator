e708884e6913117c6b5424cc5d7eca0ae845e558
kubapat@gitlab.com <Oladipo108@>
2022-10-25 01:25:50 +0200

    Implemented raytracing according to requirements

--- src/render.cpp
+++ src/render.cpp
@@ -7,16 +7,18 @@
 #include <omp.h>
 #endif
 
+#define MAX_RENDER_DEPTH 5000
+
 glm::vec3 getFinalColor(const Scene& scene, const BvhInterface& bvh, Ray ray, const Features& features, int rayDepth)
 {
     HitInfo hitInfo;
-    if (bvh.intersect(ray, hitInfo, features)) {
+    if (rayDepth <= MAX_RENDER_DEPTH && bvh.intersect(ray, hitInfo, features)) {
 
         glm::vec3 Lo = computeLightContribution(scene, bvh, features, ray, hitInfo);
 
         if (features.enableRecursive) {
             Ray reflection = computeReflectionRay(ray, hitInfo);
-            // TODO: put your own implementation of recursive ray tracing here.
+            Lo += getFinalColor(scene, bvh, reflection, features, rayDepth + 1);
         }
 
         // Draw a white debug ray if the ray hits.
